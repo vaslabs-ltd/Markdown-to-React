f51b48f14d0d0fd2806c7222084c4057b586e91f
George Onisiforou <george.onisiforou@vaslabs.io>
2022-08-02 12:33:51 +0300

    added mdx

---
+++ components/ProfileCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ProfileCard({name, photo, position, bio}) {
+  return (
+    <div className="container">
+      <div >
+        <img src={photo} alt="" className="imageContainer" ></img>
+      </div>
+      <div className="">
+        <h1 style={{color: "red"}}>{name}</h1>
+        <h2 className="profession">{position}</h2>
+        <p>{bio}</p>
+      </div>
+      
+    </div>
+  )
+}
---
+++ pages/custom-templates/test.md
@@ -0,0 +1,13 @@
+---
+
+component: employee
+name: Giakoumis
+position: Dentist
+photo: https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528
+
+---
+
+Giakoumis is a very good Dentist. He is from Australia. He never finished university but he practiced a lot on his father's crocodiles.
+
+
+
